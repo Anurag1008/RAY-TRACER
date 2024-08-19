b0cb80113c8518d9919b359c31fea7c43ea6c063
Anurag1008 <julyanurag@gmail.com>
2024-08-19 23:18:47 +0530

    commit

---
+++ build/CMakeFiles/ray-tracer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ray-tracer.dir/main.cc.o"
+  "CMakeFiles/ray-tracer.dir/main.cc.o.d"
+  "ray-tracer"
+  "ray-tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ray-tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
